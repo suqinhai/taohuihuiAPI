f92f90cea6a759ff59c7f14868b321293f40b676
suqinhai <467456744@qq.com>
2017-07-16 20:41:40 +0800

    淘宝详情页

--- core/casper/taoBaoGoodsPic.js
+++ core/casper/taoBaoGoodsPic.js
@@ -2,14 +2,14 @@ phantom.outputEncoding = "UTF-8";
 // 页面抓取数据
 
 // var data = {
-//   'goodsPics':'', //商品图片
-//   'goodsDetailsPics': '', // 商品详情
+//   'mainPic':'', //商品图片
+//   'detailsPic': '', // 商品详情
 //}
 
-// var async = require('./includes/async.js');
+
 var casper = require('casper').create({
   clientScripts: [
-    // './includes/jquery-2.2.4.js' // These two scripts will be injected in remote
+    '../includes/jquery.js' // These two scripts will be injected in remote
   ],
   pageSettings: {
     loadImages: true, // The WebPage instance used by Casper will
@@ -24,7 +24,7 @@ casper.userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML,
 
 
 var params = {
-  url: 'https://s.click.taobao.com/3EFx2gw',
+  url: 'https://s.click.taobao.com/0ogyShw',
 }
 
 casper.start();
@@ -36,19 +36,37 @@ casper.then(function() {
   var url = this.evaluate(function() {
     return window.location.href
   })
-  
-  this.scrollToBottom(2000);
+  this.scrollTo(0, 2000);
 
-  var _this = this;
-  this.waitForSelector('.ke-post', function() {
-    var size = this.evaluate(function() {
-        var img = document.getElementsByClassName('ke-post')[0].getElementsByTagName('p')[0].getElementsByTagName('img');
-        return img
-    });
-    console.log(size)
+  this.wait(100, function() {
     this.capture('google.png')
+    this.waitForSelector('.ke-post img', function() {
+      var data = this.evaluate(function() {
+        var data = {
+          mainPic: [],
+          detailsPic: [],
+        }
+        jQuery('#J_UlThumb li img').each(function() {
+          data.mainPic.push(jQuery(this).attr('src').replace('_60x60q90.jpg', '_430x430q90.jpg'))
+        });
+
+        jQuery('.ke-post img').each(function() {
+          var src = jQuery(this).attr('src')
+          if (src.indexOf('T1BYd_XwFcXXb9RTPq-90-90.png') > -1 && jQuery(this).attr('data-ks-lazyload').indexOf('spaceball.gif') == -1) {
+            data.detailsPic.push(jQuery(this).attr('data-ks-lazyload'))
+          } else if (src.indexOf('T1BYd_XwFcXXb9RTPq-90-90.png') == -1 && src.indexOf('spaceball.gif') == -1) {
+            data.detailsPic.push(src)
+          } else {
+
+          }
+        })
+
+        return data
+      });
+
+      console.log(JSON.stringify(data))
+    });
   })
 
 });
-
-casper.run()
+casper.run();
